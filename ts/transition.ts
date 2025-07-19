9867c1d9dd95ce814e3c4be3871afc4f4ff9f914
KrausC2 <curtis.kraus2002@outlook.com>
2025-07-19 21:57:26 +1000

    fix: Remove page transition on bfcache event
    Removes page transition effect from transitions that use browser's back and forward buttons as "DOMContentLoaded" would not fire, leaving page stuck between animation states.

--- ts/transition.ts
+++ ts/transition.ts
@@ -1,34 +1,29 @@
 // Handles the "fade-in" when page first loads
-
-// Applies style rules when page has loaded
-
 document.addEventListener('DOMContentLoaded', function(): void{
     // Adds the "loaded" class to the <body> when executed
     document.body.classList.add('loaded');
 });
 
+// Handles the "fade-in" when page is restored from bfcache
+window.addEventListener('pageshow', function(event: PageTransitionEvent): void {
+    // Returns true if page was restored from bfcache
+    if (event.persisted) {
+        document.body.style.transition = 'none';
+        document.body.classList.add('loaded');
+        // Wait for a brief moment before allowing transitions again
+        setTimeout(() => {
+            document.body.style.transition = '';
+        });
+    }
+});
+
 // Handles the "fade-out" when transitioning between pages
-
-// Finds all <a> tags whose attribute starts with the given name
-// ensuring only the specified tags receive the effect
-
-// .forEach() loops through every element in query and refers to
-// it as "link"
-
 document.querySelectorAll('a[href^="index"], a[href^="about"], a[href^="projects"], a[href^="contact"]').forEach((link: Element) => {
-    // Function adds an event listener which runs any time the link
-    // is clicked.
-
-    // "e" is simply just an event object called e
+    // Listens for the "click" event we call "e"
     link.addEventListener('click', function(e: Event): void {
-
-        // Prevent default event value (opening the page when
-        // clicking links)
+        // Prevent default event behavior
         e.preventDefault();
-
-        // "this" refers to the link that was clicked
-        // Gets value of href attribute (the link itself), so the URL
-        // is stored in the TARGET variable
+        // TARGET = URL of clicked link
         const TARGET = (this as HTMLAnchorElement).getAttribute('href')
         document.body.classList.remove('loaded');
         setTimeout(function(): void {
